eadea5afd9e4dc74d69b41f8d384b7f02d29be1c
Gabriel  Juan Alejandro Juhasz <88912420+GabJaJ@users.noreply.github.com>
2023-05-28 14:15:55 -0300

    Terminado
    Ultimos cambios en JavaScript

--- particulas.js
+++ particulas.js
@@ -1,54 +1,50 @@
 window.human = false;
 
-var canvasE1 = document.querySelector('.fireworks');
-var ctx = canvasE1.getContext('2d');
+var canvasEl = document.querySelector('.fireworks');
+var ctx = canvasEl.getContext('2d');
 var numberOfParticules = 30;
-var pointerx = 0;
+var pointerX = 0;
 var pointerY = 0;
 var tap = ('ontouchstart' in window || navigator.msMaxTouchPoints) ? 'touchstart' : 'mousedown';
 var colors = ['#FF1461', '#18FF92', '#5A87FF', '#FBF38C'];
 
-
-function setCanvasSize(){
-    canvasE1.width = window.innerWidth = 2;
-    canvasE1.height = window.innerHeight = 2;
-    canvasE1.style.width = window.innerWidth + 'px';
-    canvasE1.style.height = window.innerHeight + 'px';
-    canvasE1.getContext('2d').scale(2, 2);
+function setCanvasSize() {
+    canvasEl.width = window.innerWidth * 2;
+    canvasEl.height = window.innerHeight * 2;
+    canvasEl.style.width = window.innerWidth + 'px';
+    canvasEl.style.height = window.innerHeight + 'px';
+    canvasEl.getContext('2d').scale(2, 2);
 }
-function updateCoords(e){
+function updateCoords(e) {
     pointerX = e.clientX || e.touches[0].clientX;
     pointerY = e.clientY || e.touches[0].clientY;
-    }
+}
 
-
-function setParticuleDirection(p){
+function setParticuleDirection(p) {
     var angle = anime.random(0, 360) * Math.PI / 180;
     var value = anime.random(50, 180);
-    var radius = [-1, 1][anime.random(0,1)] * value;
-    return{
+    var radius = [-1, 1][anime.random(0, 1)] * value;
+    return {
         x: p.x + radius * Math.cos(angle),
         y: p.y + radius * Math.sin(angle)
     }
 }
 
-    function createParticule(x, y) {
-        var p = {};
-        p.x = x;
-        p.y = y;
-        p.color = colors[anime.random(0, colors.length - 1)];
-        p.radius = anime.random(16, 32);
-        p.endPos = setParticuleDirection(p);
-        p.draw = function () {
-            ctx.beginPath();
-            ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
-            ctx.fillStyle = p.color;
-            ctx.fill();
-        }
-        return p;
+function createParticule(x, y) {
+    var p = {};
+    p.x = x;
+    p.y = y;
+    p.color = colors[anime.random(0, colors.length - 1)];
+    p.radius = anime.random(16, 32);
+    p.endPos = setParticuleDirection(p);
+    p.draw = function () {
+        ctx.beginPath();
+        ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
+        ctx.fillStyle = p.color;
+        ctx.fill();
     }
-
-
+    return p;
+}
 
 function createCircle(x, y) {
     var p = {};
@@ -62,7 +58,7 @@ function createCircle(x, y) {
         ctx.globalAlpha = p.alpha;
         ctx.beginPath();
         ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
-        ctx.lineWidth= p.lineWidth;
+        ctx.lineWidth = p.lineWidth;
         ctx.strokeStyle = p.color;
         ctx.stroke();
         ctx.globalAlpha = 1;
@@ -70,22 +66,22 @@ function createCircle(x, y) {
     return p;
 }
 
-function renderParticule(anim){
-    for (var i = 0; i < anim.animatetables.length; i++){
-        anim.animatetables[i].target.draw();
+function renderParticule(anim) {
+    for (var i = 0; i < anim.animatables.length; i++) {
+        anim.animatables[i].target.draw();
     }
 }
 
 function animateParticules(x, y) {
     var circle = createCircle(x, y);
     var particules = [];
-    for (var i = 0; i < numberOfParticules; i++){
+    for (var i = 0; i < numberOfParticules; i++) {
         particules.push(createParticule(x, y));
     }
     anime.timeline().add({
         targets: particules,
-        x: function (p) {return p.endPos.x;},
-        y: function (p) {return p.endPos.y;},
+        x: function (p) { return p.endPos.x; },
+        y: function (p) { return p.endPos.y; },
         radius: 0.1,
         duration: anime.random(1200, 1800),
         easing: 'easeOutExpo',
@@ -108,16 +104,16 @@ function animateParticules(x, y) {
 
 var render = anime({
     duration: Infinity,
-    update: function (){
-        ctx.clearRect(0, 0, canvasE1.width, canvasE1.height);
+    update: function () {
+        ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
     }
 });
 
-document.addEventListener(tap, function (e){
+document.addEventListener(tap, function (e) {
     window.human = true;
     render.play();
     updateCoords(e);
-    animateparticulas(pointerX, pointerY);
+    animateParticules(pointerX, pointerY);
 }, false);
 
 var centerX = window.innerWidth / 2;
@@ -125,7 +121,4 @@ var centerY = window.innerHeight / 2;
 
 
 setCanvasSize();
-window.addEventListener('resize', setCanvasSize, false);
-
-
-
+window.addEventListener('resize', setCanvasSize, false);  
